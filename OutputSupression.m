9c2c3ac7e8d0fe8a254952179b2f65f2f52f3bcc
goduphi <sarkernadir.azmi@mavs.uta.edu>
2020-03-23 04:44:24 -0500

    Initial commit

---
+++ OutputSupression.m
@@ -0,0 +1,15 @@
+% Author: Sarker Nadir Afridi Azmi
+% A matlab file that illustrate ouput supression
+
+% Refer to Example1.m to learn more about house keeping
+% Some house keeping
+clear
+clc
+close all
+
+% Remove the semicolons to look at how the output changes
+Operand1 = 1;
+Operand2 = 2;
+
+% Put a semicolon after Operand2 to look at how the output changes
+Sum = Operand1 + Operand2
