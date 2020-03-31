dd8beff2f1a688e8fb52dc0eab40f16a7ed5cc0f
goduphi <sarkernadir.azmi@mavs.uta.edu>
2020-03-31 13:29:41 -0500

    Initial commit

---
+++ Review_3312020.m
@@ -0,0 +1,18 @@
+% Read the comments below and enter the required code
+
+% Clear out the command window.
+
+% Create a 2 X 3 matrix and access the element that exists at row number 1
+% and column number 2.
+
+% What happens if we try to store an integer and a string into the same
+% matrix? Show an example through coding.
+
+% How do we solve the problem associated with the above problem?
+
+% Access the 2nd element in the cell array below
+CellArrayExample = { 34 'This is a string' 3.4 };
+
+% Extend CellArrayExample to include a matrix of length 3 as the 4th
+% element and show how you would access the 3rd element of the matrix
+
