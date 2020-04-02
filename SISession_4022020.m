9a9d19c373189f100f11ad8877477f99e6d8cae2
goduphi <sarkernadir.azmi@mavs.uta.edu>
2020-04-02 14:51:32 -0500

    Initial commit

---
+++ SISession_4022020.m
@@ -0,0 +1,30 @@
+% ICA 17-22
+% House keeping
+clear
+clc
+close all % Closes all open windows
+
+% Given values
+V = 12;             % Volume [L]
+P = 2.5;            % Pressure [atm]
+MW = 28;            % Molecular Weight [g/mol]
+T = 270;            % Temperature [K]
+R = 0.08206;        % Constant
+
+% We need the number of moles because
+% the formula PV = nRT requires it
+n = (P * V) / (R * T);
+
+% Set a range for the values of x
+xT = [ 260 : 10 : 360 ];
+
+% Get another matrix with a range of values for P
+P = ((n * R)/V) * xT;
+
+figure('color', 'w');
+plot(xT, P,'-r', 'LineWidth', 2);
+grid on
+title('Theoretical relationship of an ideal gas');
+xlabel('Temperature (T) [K]');
+ylabel('Pressure (P) [atm]');
+axis([260 360 2.5 3.5]);
